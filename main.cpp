b304cbbcc5b7dda37915f24f60d89906e8522454
BaekGyu Han <white_gyu@naver.com>
2022-01-28 13:53:33 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,18 @@
 #include "SerialUtil.h"
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-int main()
-{
-	//Don't forget to handle exceptions and throw suitable messages as we are dealing with serial read/write
-	try {
-		//create an instance of SerialUtil object
-		SerialUtil* su = new SerialUtil();
-		string str;
-		while (SerialUtil::SP->IsConnected())
-		{
-			//read string from commandLine
-			getline(cin, str);
-			//write the string read from commandLine
-			su->write(str);
-		}
+int main() {
+	SerialUtil* su = new SerialUtil();
+	string str;
+
+	while (SerialUtil::SP->IsConnected()) {
+		getline(cin, str);
+
+		su->write(str);
 	}
-	catch (string s)
-	{
-		cout << "..ERROR OCCURED::" << s << " :( \n";
-	}
+	
 	return 0;
 }
