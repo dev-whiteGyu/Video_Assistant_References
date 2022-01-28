579094832750c3687397654465acc8cb8cc328ca
BaekGyu Han <white_gyu@naver.com>
2022-01-28 13:52:37 +0900

    Update openCamera.cpp

--- openCamera.cpp
+++ openCamera.cpp
@@ -9,24 +9,21 @@ using namespace cv;
 using namespace std;
 
 
-int main()
-{
-
+int main() {
 	Mat frame;
 
 	VideoCapture cap("output.avi");
-	if (!cap.isOpened())
-	{
+	
+	if (!cap.isOpened()) {
 		cerr << "카메라를 열 수 없습니다." << endl;
 		return -1;
 	}
 
 
-	while (1)
-	{
+	while (1) {
 		cap.read(frame);
-		if (frame.empty())
-		{
+		
+		if (frame.empty()) {
 			cerr << "캡쳐 실패" << endl;
 			break;
 		}
@@ -34,10 +31,8 @@ int main()
 
 		imshow("Live", frame);
 
-		if (waitKey(30) >= 0)
-			break;
+		if (waitKey(30) >= 0) break;
 	}
-
 
 	return 0;
 }
